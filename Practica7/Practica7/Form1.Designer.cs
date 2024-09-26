e0f745ae761fa9dac7f127046ac0c93e19d6315b
enrique Gudiño <137984675+Catrin101@users.noreply.github.com>
2024-09-25 22:22:23 -0700

    subida practica 7

---
+++ Practica7/Practica7/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Practica7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.du = new System.Windows.Forms.Button();
+            this.re = new System.Windows.Forms.Button();
+            this.mi = new System.Windows.Forms.Button();
+            this.fa = new System.Windows.Forms.Button();
+            this.sol = new System.Windows.Forms.Button();
+            this.la = new System.Windows.Forms.Button();
+            this.si = new System.Windows.Forms.Button();
+            this.duuu = new System.Windows.Forms.Button();
+            this.doo = new System.Windows.Forms.Button();
+            this.faa = new System.Windows.Forms.Button();
+            this.soll = new System.Windows.Forms.Button();
+            this.LAA = new System.Windows.Forms.Button();
+            this.ree = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // du
+            // 
+            this.du.BackColor = System.Drawing.SystemColors.Control;
+            this.du.Location = new System.Drawing.Point(0, 0);
+            this.du.Name = "du";
+            this.du.Size = new System.Drawing.Size(100, 438);
+            this.du.TabIndex = 0;
+            this.du.Text = "DO (A)";
+            this.du.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.du.UseVisualStyleBackColor = false;
+            this.du.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // re
+            // 
+            this.re.BackColor = System.Drawing.SystemColors.Control;
+            this.re.Location = new System.Drawing.Point(106, 0);
+            this.re.Name = "re";
+            this.re.Size = new System.Drawing.Size(100, 438);
+            this.re.TabIndex = 1;
+            this.re.Text = "RE (S)";
+            this.re.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.re.UseVisualStyleBackColor = false;
+            this.re.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // mi
+            // 
+            this.mi.BackColor = System.Drawing.SystemColors.Control;
+            this.mi.Location = new System.Drawing.Point(212, 0);
+            this.mi.Name = "mi";
+            this.mi.Size = new System.Drawing.Size(100, 438);
+            this.mi.TabIndex = 2;
+            this.mi.Text = "MI (D)";
+            this.mi.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.mi.UseVisualStyleBackColor = false;
+            this.mi.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // fa
+            // 
+            this.fa.BackColor = System.Drawing.SystemColors.Control;
+            this.fa.Location = new System.Drawing.Point(318, 0);
+            this.fa.Name = "fa";
+            this.fa.Size = new System.Drawing.Size(100, 438);
+            this.fa.TabIndex = 3;
+            this.fa.Text = "FA (F)";
+            this.fa.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.fa.UseVisualStyleBackColor = false;
+            this.fa.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // sol
+            // 
+            this.sol.BackColor = System.Drawing.SystemColors.Control;
+            this.sol.Location = new System.Drawing.Point(424, 0);
+            this.sol.Name = "sol";
+            this.sol.Size = new System.Drawing.Size(100, 438);
+            this.sol.TabIndex = 4;
+            this.sol.Text = "SOL (G)";
+            this.sol.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.sol.UseVisualStyleBackColor = false;
+            this.sol.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // la
+            // 
+            this.la.BackColor = System.Drawing.SystemColors.Control;
+            this.la.Location = new System.Drawing.Point(530, 0);
+            this.la.Name = "la";
+            this.la.Size = new System.Drawing.Size(100, 438);
+            this.la.TabIndex = 5;
+            this.la.Text = "LA (H)";
+            this.la.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.la.UseVisualStyleBackColor = false;
+            this.la.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // si
+            // 
+            this.si.BackColor = System.Drawing.SystemColors.Control;
+            this.si.Location = new System.Drawing.Point(636, 0);
+            this.si.Name = "si";
+            this.si.Size = new System.Drawing.Size(100, 438);
+            this.si.TabIndex = 6;
+            this.si.Text = "SI (J)";
+            this.si.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.si.UseVisualStyleBackColor = false;
+            this.si.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // duuu
+            // 
+            this.duuu.BackColor = System.Drawing.SystemColors.Control;
+            this.duuu.Location = new System.Drawing.Point(742, 0);
+            this.duuu.Name = "duuu";
+            this.duuu.Size = new System.Drawing.Size(100, 438);
+            this.duuu.TabIndex = 7;
+            this.duuu.Text = "DO (K)";
+            this.duuu.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.duuu.UseVisualStyleBackColor = false;
+            this.duuu.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // doo
+            // 
+            this.doo.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.doo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.doo.Location = new System.Drawing.Point(54, 12);
+            this.doo.Name = "doo";
+            this.doo.Size = new System.Drawing.Size(100, 250);
+            this.doo.TabIndex = 8;
+            this.doo.Text = "DO# (W)";
+            this.doo.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.doo.UseVisualStyleBackColor = false;
+            this.doo.Click += new System.EventHandler(this.doo_Click);
+            // 
+            // faa
+            // 
+            this.faa.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.faa.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.faa.Location = new System.Drawing.Point(370, 12);
+            this.faa.Name = "faa";
+            this.faa.Size = new System.Drawing.Size(100, 250);
+            this.faa.TabIndex = 10;
+            this.faa.Text = "FA# (T)";
+            this.faa.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.faa.UseVisualStyleBackColor = false;
+            this.faa.Click += new System.EventHandler(this.faa_Click);
+            // 
+            // soll
+            // 
+            this.soll.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.soll.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.soll.Location = new System.Drawing.Point(476, 12);
+            this.soll.Name = "soll";
+            this.soll.Size = new System.Drawing.Size(100, 250);
+            this.soll.TabIndex = 11;
+            this.soll.Text = "SOL# (Y)";
+            this.soll.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.soll.UseVisualStyleBackColor = false;
+            this.soll.Click += new System.EventHandler(this.soll_Click);
+            // 
+            // LAA
+            // 
+            this.LAA.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.LAA.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LAA.Location = new System.Drawing.Point(582, 12);
+            this.LAA.Name = "LAA";
+            this.LAA.Size = new System.Drawing.Size(100, 250);
+            this.LAA.TabIndex = 12;
+            this.LAA.Text = "LA# (U)";
+            this.LAA.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.LAA.UseVisualStyleBackColor = false;
+            this.LAA.Click += new System.EventHandler(this.LAA_Click);
+            // 
+            // ree
+            // 
+            this.ree.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ree.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ree.Location = new System.Drawing.Point(160, 12);
+            this.ree.Name = "ree";
+            this.ree.Size = new System.Drawing.Size(100, 250);
+            this.ree.TabIndex = 9;
+            this.ree.Text = "RE# (E)";
+            this.ree.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.ree.UseVisualStyleBackColor = false;
+            this.ree.Click += new System.EventHandler(this.ree_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(874, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LAA);
+            this.Controls.Add(this.soll);
+            this.Controls.Add(this.faa);
+            this.Controls.Add(this.ree);
+            this.Controls.Add(this.doo);
+            this.Controls.Add(this.duuu);
+            this.Controls.Add(this.si);
+            this.Controls.Add(this.la);
+            this.Controls.Add(this.sol);
+            this.Controls.Add(this.fa);
+            this.Controls.Add(this.mi);
+            this.Controls.Add(this.re);
+            this.Controls.Add(this.du);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button du;
+        private System.Windows.Forms.Button re;
+        private System.Windows.Forms.Button mi;
+        private System.Windows.Forms.Button fa;
+        private System.Windows.Forms.Button sol;
+        private System.Windows.Forms.Button la;
+        private System.Windows.Forms.Button si;
+        private System.Windows.Forms.Button duuu;
+        private System.Windows.Forms.Button doo;
+        private System.Windows.Forms.Button faa;
+        private System.Windows.Forms.Button soll;
+        private System.Windows.Forms.Button LAA;
+        private System.Windows.Forms.Button ree;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Practica7/Practica7/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica7{
+    public partial class Form1 : Form{
+        private Dictionary<string, string> Sonidos;
+        private Dictionary<Keys, string> MapeoTeclas;
+        public Form1(){
+            InitializeComponent();
+            Sonidos = new Dictionary<string, string>{
+                {"Do#",@"Sonidos\DO#.wav"},
+                {"Do",@"Sonidos\DO.wav"},
+                {"DoUltimo",@"Sonidos\DoUltimo.wav"},
+                {"Fa#",@"Sonidos\FA#.wav"},
+                {"Fa",@"Sonidos\FA.wav"},
+                {"La#",@"Sonidos\LA#.wav"},
+                {"La",@"Sonidos\LA.wav"},
+                {"Mi",@"Sonidos\MI.wav"},
+                {"Re#",@"Sonidos\RE#.wav"},
+                {"Re",@"Sonidos\RE.wav"},
+                {"Si",@"Sonidos\SI.wav"},
+                {"Sol#",@"Sonidos\SOL#.wav"},
+                {"Sol",@"Sonidos\SOL.wav"}
+            };
+            MapeoTeclas = new Dictionary<Keys, string> {
+                { Keys.A, "Do" },
+                { Keys.W, "Do#" },
+                { Keys.S, "Re" },
+                { Keys.E, "Re#" },
+                { Keys.D, "Mi" },
+                { Keys.F, "Fa" },
+                { Keys.T, "Fa#" },
+                { Keys.G, "Sol" },
+                { Keys.Y, "Sol#" },
+                { Keys.H, "La" },
+                { Keys.U, "La#" },
+                { Keys.J, "Si" },
+                { Keys.K, "DoUltimo"}
+            };
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+            this.KeyPreview = true;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) {
+            if (MapeoTeclas.ContainsKey(e.KeyCode)) {
+                string nota = MapeoTeclas[e.KeyCode];
+                label1.Text = nota;
+                ReproducirSonidos (nota);
+            }
+        }
+
+        public void ReproducirSonidos(string sonido) {
+            if (Sonidos.ContainsKey(sonido)) { 
+                SoundPlayer player = new SoundPlayer(Sonidos[sonido]);
+                label1.Text = sonido;
+                player.Play();
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ReproducirSonidos("Sol");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ReproducirSonidos("Si");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ReproducirSonidos("La");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ReproducirSonidos("Fa");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ReproducirSonidos("DoUltimo");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ReproducirSonidos("Mi");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ReproducirSonidos("Re");
+        }
+
+        private void button1_Click(object sender, EventArgs e){
+            ReproducirSonidos("Do");
+        }
+
+        private void doo_Click(object sender, EventArgs e)
+        {
+            ReproducirSonidos("Do#");
+        }
+
+        private void ree_Click(object sender, EventArgs e)
+        {
+            ReproducirSonidos("Re#");
+        }
+
+        private void faa_Click(object sender, EventArgs e)
+        {
+            ReproducirSonidos("Fa#");
+        }
+
+        private void soll_Click(object sender, EventArgs e)
+        {
+            ReproducirSonidos("Sol#");
+        }
+
+        private void LAA_Click(object sender, EventArgs e)
+        {
+            ReproducirSonidos("La#");
+        }
+    }
+}
