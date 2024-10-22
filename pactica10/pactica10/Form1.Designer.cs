dede10b82c9d50fdfcb0569a01d71d8afdbc63e0
enrique Gudiño <137984675+Catrin101@users.noreply.github.com>
2024-10-22 16:08:23 -0700

    practica 10
    subida practica 10

---
+++ pactica10/pactica10/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace pactica10
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.btn_IniciarS = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txt_Usuario = new System.Windows.Forms.TextBox();
+            this.txt_Contraseña = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.lbl_Usuario = new System.Windows.Forms.Label();
+            this.lbl_Contraseña = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.menError = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_IniciarS
+            // 
+            this.btn_IniciarS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_IniciarS.Location = new System.Drawing.Point(227, 339);
+            this.btn_IniciarS.Name = "btn_IniciarS";
+            this.btn_IniciarS.Size = new System.Drawing.Size(112, 33);
+            this.btn_IniciarS.TabIndex = 0;
+            this.btn_IniciarS.Text = "Iniciar Sesios";
+            this.btn_IniciarS.UseVisualStyleBackColor = true;
+            this.btn_IniciarS.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(212, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txt_Usuario
+            // 
+            this.txt_Usuario.Location = new System.Drawing.Point(248, 214);
+            this.txt_Usuario.Name = "txt_Usuario";
+            this.txt_Usuario.Size = new System.Drawing.Size(114, 22);
+            this.txt_Usuario.TabIndex = 2;
+            this.txt_Usuario.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txt_Contraseña
+            // 
+            this.txt_Contraseña.Location = new System.Drawing.Point(248, 285);
+            this.txt_Contraseña.Name = "txt_Contraseña";
+            this.txt_Contraseña.Size = new System.Drawing.Size(114, 22);
+            this.txt_Contraseña.TabIndex = 3;
+            this.txt_Contraseña.UseSystemPasswordChar = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(212, 206);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(212, 277);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // lbl_Usuario
+            // 
+            this.lbl_Usuario.AutoSize = true;
+            this.lbl_Usuario.Location = new System.Drawing.Point(209, 187);
+            this.lbl_Usuario.Name = "lbl_Usuario";
+            this.lbl_Usuario.Size = new System.Drawing.Size(54, 16);
+            this.lbl_Usuario.TabIndex = 6;
+            this.lbl_Usuario.Text = "Usuario";
+            // 
+            // lbl_Contraseña
+            // 
+            this.lbl_Contraseña.AutoSize = true;
+            this.lbl_Contraseña.Location = new System.Drawing.Point(209, 258);
+            this.lbl_Contraseña.Name = "lbl_Contraseña";
+            this.lbl_Contraseña.Size = new System.Drawing.Size(76, 16);
+            this.lbl_Contraseña.TabIndex = 7;
+            this.lbl_Contraseña.Text = "Contraseña";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(248, 313);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 20);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Mostrar";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // menError
+            // 
+            this.menError.AutoSize = true;
+            this.menError.Location = new System.Drawing.Point(265, 399);
+            this.menError.Name = "menError";
+            this.menError.Size = new System.Drawing.Size(0, 16);
+            this.menError.TabIndex = 9;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 553);
+            this.Controls.Add(this.menError);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lbl_Contraseña);
+            this.Controls.Add(this.lbl_Usuario);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.txt_Contraseña);
+            this.Controls.Add(this.txt_Usuario);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_IniciarS);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_IniciarS;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txt_Usuario;
+        private System.Windows.Forms.TextBox txt_Contraseña;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label lbl_Usuario;
+        private System.Windows.Forms.Label lbl_Contraseña;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label menError;
+    }
+}
+
---
+++ pactica10/pactica10/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pactica10{
+    public partial class Login : Form{
+        public Login(){
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e){
+            string usuarioCorrecto = "enrique";
+            string passwordCorrecta = "0531";
+
+            string usuarioIngresado = txt_Usuario.Text;
+            string passwordIngresada = txt_Contraseña.Text;
+
+            if (usuarioIngresado == usuarioCorrecto && passwordIngresada == passwordCorrecta){
+                Form2 Contenido = new Form2();
+                Contenido.Show();
+                this.Hide();
+            }else{
+                if (usuarioIngresado != usuarioCorrecto && passwordIngresada != passwordCorrecta){
+                    menError.Text = "Usuario y contraseña incorrectos";
+                }else if (usuarioIngresado != usuarioCorrecto){
+                    menError.Text = "Usuario incorrecto";
+                }else{
+                    menError.Text = "Contraseña incorrecta";
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true){
+                txt_Contraseña.UseSystemPasswordChar = false;
+            }if (checkBox1.Checked == false){
+                txt_Contraseña.UseSystemPasswordChar = true;
+            }
+        }
+    }
+}
